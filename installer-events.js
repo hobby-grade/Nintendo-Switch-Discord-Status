bac70193cde5d36774bf5fd03af05a85bd762161
Devon <devon@da532.com>
2019-05-06 00:40:37 +0100

    ..oh yeah that exists

--- installer-events.js
+++ installer-events.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-empty */
 /* eslint-disable no-unused-vars */
+
 // Windows stuff
-// eslint-disable-next-line no-unused-vars
 function handleSquirrelEvent(app) {
     if (process.argv.length === 1) {
         return false;
@@ -28,25 +28,14 @@ function handleSquirrelEvent(app) {
     switch (squirrelEvent) {
         case '--squirrel-install':
         case '--squirrel-updated':
-            // Optionally do things such as:
-            // - Add your .exe to the PATH
-            // - Write to the registry for things like file associations and
-            //   explorer context menus
-            // Install desktop and start menu shortcuts
             spawnUpdate(['--createShortcut', exeName]);
             setTimeout(app.quit, 1000);
             return true;
         case '--squirrel-uninstall':
-            // Undo anything you did in the --squirrel-install and
-            // --squirrel-updated handlers
-            // Remove desktop and start menu shortcuts
             spawnUpdate(['--removeShortcut', exeName]);
             setTimeout(app.quit, 1000);
             return true;
         case '--squirrel-obsolete':
-            // This is called on the outgoing version of your app before
-            // we update to the new version - it's the opposite of
-            // --squirrel-updated
             app.quit();
             return true;
     }
